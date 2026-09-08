d60c1f4e94e4207f87967c033ff78cd18667439e
Jonathan <jonathanarteta24@gmail.com>
2026-09-08 15:32:32 +0800

    Initial commit

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,11 @@
+import './NavigationBar.jsx'
+
+function App() {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import NavigationBar from './components/NavigationBar.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <NavigationBar />
+  </StrictMode>,
+)

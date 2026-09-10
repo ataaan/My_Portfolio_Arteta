83e6d11316c0315f5dc8177b36b32d1160b43cbe
Jonathan <jonathanarteta24@gmail.com>
2026-09-10 14:01:22 +0800

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -30,6 +30,15 @@ function App() {
               More about me
             </button>
         </div>
+
+    <div class = "social-container">
+          <img src="/assets/fb.png" className="fb-image" alt="fb"/>
+          <img src="/assets/instagram.png" className="instagram-image" alt="instagram"/>
+          <img src="/assets/gmail.png" className="gmail-image" alt="gmail"/>
+          <img src="/assets/linkedin.png" className="linkedin-image" alt="linkedin"/>
+          <img src="/assets/github.png" className="github-image" alt="github"/>
+          
+    </div> 
         
     <div class = "img-container">
           <img src="/assets/arteta.png" className="person-image" alt="Jonathan"/>

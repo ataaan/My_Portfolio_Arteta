946ab6bb83b517d900bfc6858cc46b7a90cb9de4
Jonathan <jonathanarteta24@gmail.com>
2026-09-16 10:05:59 +0800

    added active navigation

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 function NavigationBar() {
   return (
     <nav className="NavigationBar">
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/project">Project</Link>
-      <Link to="/contact">Contact</Link>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/project">Project</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
     </nav>
   )
 }

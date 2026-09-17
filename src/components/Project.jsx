37cab9593cd261ac093600d7f6b7c8f12607939c
Jonathan <jonathanarteta24@gmail.com>
2026-09-17 18:33:03 +0800

    Projects added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,35 @@
+import { Link } from 'react-router-dom'
+import "../style/Project.css"
+
+
 function Project() {
   return (
-    <div>
-      <h1>Project</h1>
-      <p>This is my about page.</p>
-    </div>
+    <>
+
+    <h1 className = "page-title">Project</h1>
+
+    <div className = "web-dev-proj-container">
+      <p className = "proj-title-desc">Web Development</p>
+      
+      <p className = "prog-name">Brgy<span>X</span>press</p>
+      
+      <p className = "web-dev-proj-desc">
+        A web-based platform that simplifies barangay document request by allowing 
+        residents to request, track, and receive documents online through doorstep 
+        delivery. It help modernize barangay services  by reducing manual transactions, 
+        waiting times and unnecessary visits to the barangay office.
+      </p>
+
+      <div class = "proj-container">
+        <img src="/assets/arteta-about.png" className="website-img1" alt="Jonathan-about"/>
+      </div>
+
+      <Link className="view-website-button">
+        View Website
+      </Link>   
+
+    </div> 
+    </>
   );
 }
 
